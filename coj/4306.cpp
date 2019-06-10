cab396a9e161a7d7001b348e3f773d551b808cd3
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2019-06-09 22:15:31 -0500

    coj

--- coj/4306.cpp
+++ coj/4306.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 int main() {
-    long long n, a, b, x, y, z;
-    scanf("%lld\n", &n);
+    int n, a, b, cr[3];
+    scanf("%d", &n);
     while (n--) {
-        scanf("%lld%lld%lld%lld%lld", &a, &b, &x, &y, &z);
-        long long sp = x * x + y * y + z * z;
-        sp += sp;
-        if (a * b >= sp) printf("POSSIBLE\n");
+        scanf("%d%d%d%d%d", &a, &b, cr, cr + 1, cr + 2);
+        sort(cr, cr + 3);
+        bool ans = false;
+        do {
+            int w = cr[0] + cr[1] + cr[2] * 2;
+            int h = cr[0] * 2 + cr[1];
+            if ((h <= a && w <= b) || (h <= b && w <= a)) ans = true;
+            w = cr[0] + cr[1] * 2 + cr[2];
+            h = cr[0] + cr[1] + cr[2];
+            if ((h <= a && w <= b) || (h <= b && w <= a)) ans = true;
+            w = cr[1] * 2 + cr[2] * 2;
+            h = cr[0] + cr[2] * 2;
+            if ((h <= a && w <= b) || (h <= b && w <= a)) ans = true;
+            w = cr[0] + cr[1] * 3 + cr[2];
+            h = cr[0] + cr[2];
+            if ((h <= a && w <= b) || (h <= b && w <= a)) ans = true;
+        } while (next_permutation(cr, cr + 3));
+        if (ans) printf("POSSIBLE\n");
         else printf("IMPOSSIBLE\n");
     }
     return 0;
---
+++ coj/4316.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    long long m, ms[100005];
+    while (scanf("%d%lld", &n, &m) != EOF) {
+        int l = 0, r = 0, al = -1, ar = -1;
+        long long sm = 0;
+        for (; r < n; r++) {
+            scanf("%lld", ms + r);
+            sm += ms[r];
+            while (sm > m)
+                sm -= ms[l++];
+            if (sm == m && al == -1) {
+                al = l + 1;
+                ar = r + 1;
+            }
+        }
+        printf("%d %d\n", al, ar);
+    }
+    return 0;
+}
