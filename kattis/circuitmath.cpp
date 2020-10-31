b47577e2fd71cc7f1f205b488d4bff83f6841c1f
benilda coari condo <cbcesia10@gmail.com>
2020-10-30 21:27:22 -0500

    added spoj and kattis link problems closes #7

--- kattis/circuitmath.cpp
+++ kattis/circuitmath.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+// Problem link: https://open.kattis.com/problems/circuitmath
+#include <bits/stdc++.h>
 
 using namespace std;
 
--- spoj/ACODE.cpp
+++ spoj/ACODE.cpp
@@ -1,3 +1,5 @@
+// Problem link: https://www.spoj.com/problems/ACODE/
+
 #include <bits/stdc++.h>
 
 using namespace std;
